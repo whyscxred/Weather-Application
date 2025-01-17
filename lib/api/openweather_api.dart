1bb31b886d245fcbfe81d7f99ca28a20d2066e91
whyscxred <152486204+whyscxred@users.noreply.github.com>
2025-01-17 18:28:50 +0500

    API update

--- lib/api/openweather_api.dart
+++ lib/api/openweather_api.dart
@@ -1,7 +1,7 @@
 
 class OpenWeatherAPI {
 
-  String apiKey = 'e42a35e2c5e74953592b9fe5fa0fa792';
+  String apiKey = 'Нужно вставить сюда апи ключ';
 
   String apiUrl(lat, lon) {
     return 'https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric&exclude=minutely';
